ed75dfac5f8fcaf8cbd0c52a3a7381887b017d57
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-06-29 11:10:39 +0200

    add node version info

--- program.js
+++ program.js
@@ -1,18 +1,25 @@
 process.stdin.setEncoding('utf-8');
-process.stdout.write('Hello,chose 1 or 2')
+process.stdout.write('Hello,chose 1,2,3 or /exit' + '\n')
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
    		switch(instruction){
-   			case "1":
-   				process.stdout.write('you chose 1'+ "<br>")
+   			case '1':
+   				process.stdout.write('you chose 1'+ '\n')
    				break;
-   			case "2":
-   				console.log(process.env)
+   			case '2':
+   				console.log(process.env.lang)
+   				break;
+   			case '3':
+   				console.log(process.versions.node)
+   				break;
+   			  case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
    				break;
    			default:
-   				process.stdout.write('type something else' + "<br>")
+   				process.stdout.write('type something else' + '\n')
    				break;
    		}
     }
